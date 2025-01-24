057aa05c580be284b510f5768a4bb53281e984d2
Larry Kann <lawrencekann@tuta.com>
2025-01-24 13:53:53 -0500

    the neovim setup

---
+++ nvim/lua/atariaf/plugins/lsp.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        ft = "lua", -- only load on lua files
+        opts = {
+          library = {
+            -- See the configuration section for more details
+            -- Load luvit types when the `vim.uv` word is found
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      -- Lua LSP
+      lspconfig.lua_ls.setup {}
+
+      -- C LSP
+      lspconfig.clangd.setup {
+        cmd = {
+          "clangd"
+        },
+      }
+    end,
+  }
+}
