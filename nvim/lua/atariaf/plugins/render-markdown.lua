bbc44e75a5258527c77d653c66713f3433ed0fcc
Larry Kann <lawrencekann@tuta.com>
2025-02-13 07:23:06 -0500

    still messing with markdown, placed on hold as is for now

--- nvim/lua/atariaf/plugins/render-markdown.lua
+++ nvim/lua/atariaf/plugins/render-markdown.lua
@@ -2,17 +2,14 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+
     ---@module 'render-markdown'
     ---@type render.md.UserConfig
-    opts = {},
-    config = function()
-      require('render-markdown').setup {
-        paragraph = {
-          left_margin = 0.5,
-          min_width = 30,
-        },
-      }
-    end
-
+    opts = {
+      paragraph = {
+        left_margin = 0.5,
+        min_width = 30,
+      },
+    },
   }
 }
