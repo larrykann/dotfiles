c35ee3044765a77d17bb00691082bc87de54df44
Larry Kann <lawrencekann@tuta.com>
2025-03-17 16:14:28 -0400

    added rofi to symlink script

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ ln -sf "$DOTFILES_DIR/polybar" "$HOME/.config/polybar"
 ln -sf "$DOTFILES_DIR/wezterm" "$HOME/.config/wezterm"
 ln -sf "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
 ln -sf "$DOTFILES_DIR/tmux" "$HOME/.config/tmux"
+ln -sf "$DOTFILES_DIR/rofi" "$HOME/.config/rofi"
 
 # Example of a dotfile that doesn't live in .config
 # ln -sf "$DOTFILES_DIR/bash/.bashrc" "$HOME/.bashrc"
